99d9679d359e8e9a06ab6e1aa2977aafedbe97fd
mohamed hisham ismail <48564419+kingusa1@users.noreply.github.com>
2025-08-28 12:20:59 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,13 +11,6 @@ export const metadata: Metadata = {
   },
   description: 'Empowering every organization and individual with limitless, intuitive automation solutions—driving efficiency, innovation, and seamless operations across all digital channels.',
   keywords: ['automation', 'B2B SaaS', 'workflow automation', 'CRM integration', 'social media automation', 'digital platform automation', 'custom workflows', 'Infiniteo'],
-  icons: {
-    icon: {
-      url: '/infiniteo.png',
-      type: 'image/png',
-      sizes: 'any',
-    },
-  },
 };
 
 export default function RootLayout({
